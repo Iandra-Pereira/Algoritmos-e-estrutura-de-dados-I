06a5dfb7526d0f2956f325caab89607380e13439
Iandra <iandra.sousa@alunos.ufersa.edu.br>
2026-09-21 20:38:53 -0300

    att
    This program prompts for employee details and writes them to a file.

---
+++ arquivofuncionario.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    FILE * arq;
+    arq = fopen("arquivo.txt", "w");
+    if(arq == NULL){
+        printf("Erro ao abrir o arquivo!\n");
+        exit(1);
+    }else{
+        printf("Arquivo aberto com sucesso!\n");
+    }
+
+    int id;
+    char nome[50];
+    float salario;
+    printf("Digite o ID do funcionário: ");
+    scanf("%d", &id);   
+    printf("Digite o nome do funcionário: ");
+    scanf(" %[^\n]", nome);
+    printf("Digite o salário do funcionário: ");
+    scanf("%f", &salario);
+
+    fprintf(arq, "%d %s %.2f\n", id, nome, salario);
+    fclose(arq);
+}
